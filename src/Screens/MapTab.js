89fa4ed8e6b0c361c61601b98843cf21c22d2d60
jorgeFaleiros <jorge.faleiros@luizalabs.com>
2017-09-25 13:44:17 -0300

    ajuste de mapa

--- src/Screens/MapTab.js
+++ src/Screens/MapTab.js
@@ -11,37 +11,57 @@ import MapView from 'react-native-maps';
 class MapTab extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.04,
+        longitudeDelta: 0.04
+      }
+    }
+    this.onRegionChange = this.onRegionChange.bind(this);
   }
   componentWillMount() {
-    let position = navigator.geolocation.getCurrentPosition().then((result)=>{
+    navigator.geolocation.getCurrentPosition((position) => {
       this.setState({
-        position: result
+        region: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+          error: null,
+        }
       });
-      console.log(result);
+      console.log(this.state);
     });
+
   }
   static navigationOptions = {
     title: 'Teste tab ',
+  }
+  onRegionChange(region) {
+    this.setState({ region });
   }
   render() {
     return (
       <View style={{flex:1}}>
         <MapView
           style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0
+            flex:1
           }}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.04,
-            longitudeDelta: 0.04
-          }}
+          initialRegion={this.state.region}
+          onRegionChange={this.onRegionChange}
+          region={this.state.region}
         />
-
+        <MapView.Marker
+          key={1}
+          coordinate={{
+            latitude:this.state.region.latitude,
+            longitude:this.state.region.longitude
+          }}
+          title='Você esta aqui!'
+          description='Você está aqui!'
+        ></MapView.Marker>
       </View>
     );
   }
