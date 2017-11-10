c344e02c1468693af3394bd696881ad875c3ec81
jorgeFaleiros <jorge.faleiros@luizalabs.com>
2017-11-10 14:10:47 -0200

    add markers

--- src/Screens/MapTab.js
+++ src/Screens/MapTab.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 import MapView from 'react-native-maps';
 
+const { width, height } = Dimensions.get('window');
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const ASPECT_RATIO = width / height;
+const LATITUDE = -20.540158;
+const LONGITUDE = -47.405136;
+const LATITUDE_DELTA = 0.04;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
 
 class MapTab extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.08,
-        longitudeDelta: 0.08
-      }
-    }
-    this.onRegionChange = this.onRegionChange.bind(this);
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markers: [],
+    };
   }
   componentWillMount() {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -33,7 +46,33 @@ class MapTab extends Component {
         }
       });
     });
-
+  }
+  onMapPress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: id++,
+          color: randomColor(),
+        },
+      ],
+    });
+  }
+  componentDidMount() {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: {
+            latitude: -20.540158,
+            longitude: -47.405136,
+          },
+          key: id++,
+          color: randomColor(),
+        },
+      ],
+    });
   }
   static navigationOptions = {
     title: 'Casos na regiãos',
@@ -43,26 +82,67 @@ class MapTab extends Component {
   }
   render() {
     return (
-      <View style={{flex:1}}>
+      <View style={styles.container}>
         <MapView
-          style={{
-            flex:1
-          }}
+          provider={this.props.provider}
+          style={styles.map}
           initialRegion={this.state.region}
-          onRegionChange={this.onRegionChange}
-          region={this.state.region}
-        />
-        <MapView.Marker
-          key={1}
-          coordinate={{
-            latitude:this.state.region.latitude,
-            longitude:this.state.region.longitude
-          }}
-          title='Você esta aqui!'
-          description='Você está aqui!'
-        ></MapView.Marker>
+          onPress={(e) => this.onMapPress(e)}
+        >
+          {this.state.markers.map(marker => (
+            <MapView.Marker
+              key={marker.key}
+              coordinate={marker.coordinate}
+              pinColor={marker.color}
+            />
+          ))}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.setState({ markers: [] })}
+            style={styles.bubble}
+          >
+            <Text>Tap to create a marker of random color</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
 }
+MapTab.propTypes = {
+  provider: MapView.ProviderPropType,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
+  },
+});
+
 export default MapTab;
